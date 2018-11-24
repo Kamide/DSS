a312f2481bd85c6f2f636f01a51aceccf7279827
Arafat Arman <ayyarman32o@gmail.com>
2018-11-24 17:32:12 -0500

    Merge branch 'arafat_local_changes2'

--- msg_system/urls.py
+++ msg_system/urls.py
@@ -3,5 +3,8 @@ from msg_system import views
 
 urlpatterns = [
     # /msg_system/compose/
-    path('compose/', views.compose, name = 'compose')
+    path('compose/', views.compose, name = 'compose'),
+
+    # /msg_system/mailbox/
+    path('mailbox/', views.mailbox, name = 'mailbox')
 ]
