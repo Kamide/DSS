566857cda16de265d06c568302f2ed29b59c58ac
Arafat Arman <ayyarman32o@gmail.com>
2018-11-24 18:10:24 -0500

    BruteForce Restore
    By the power of copy pasting, our old DB should be back bois

--- msg_system/models.py
+++ msg_system/models.py
@@ -5,7 +5,9 @@ from django.contrib.auth.models import User
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "sender")
     receiver = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "receiver")
-    msg_content = models.TextField(help_text = 'Type message here')
+    msg_content = models.TextField(default = 'Type message here')
 
     def __str__(self):
         return self.sender.username + ' ' + self.receiver.username + ' ' + self.msg_content
+
+
