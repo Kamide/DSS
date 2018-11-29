95823f4b5fcd58cee80a20e997da7b388451c7a9
Arafat Arman <ayyarman32o@gmail.com>
2018-11-29 13:52:58 -0500

    Update Taboo Add/Delete
    -SU can now add or delete multiple space separated words to/from the taboo list.

--- taboo/views.py
+++ taboo/views.py
@@ -8,17 +8,34 @@ def add_word(request):
     if request.method == "POST":
         taboo_form = TabooForm(request.POST)
         if taboo_form.is_valid():
-            t = taboo_form.save(commit = False)
-            word_to_add = t.word
-            match_qs = Taboo.objects.all().filter(word = word_to_add)
-            if match_qs.count() > 0:
-                messages.error(request, f'The word you entered is already on the list')
+            t = taboo_form.save(commit=False)
+            words_to_add = t.word.split()
+            if len(words_to_add) == 1:
+                match_qs = Taboo.objects.all().filter(word=words_to_add[0])
+                if match_qs.count() > 0:
+                    messages.error(request, f'The word you entered is already on the list')
+                else:
+                    taboo_form.save()
+                    messages.success(request, f'The word {words_to_add[0]} has been added to the list')
             else:
-                taboo_form.save()
-                word_added = taboo_form.cleaned_data.get('word')
-                messages.success(request, f'The word {word_added} has been added to the list')
-                return redirect('/')
-
+                duplicate_words = list()
+                added_words = list()
+                for w in words_to_add:
+                    match_qs = Taboo.objects.all().filter(word=w)
+                    if match_qs.count() == 0:
+                        added_words.append(w)
+                        t = Taboo(word=w)
+                        t.save()
+                    else:
+                        duplicate_words.append(w)
+                if len(added_words) > 0:
+                    messages.success(request, f'The word(s) {added_words} have been added to the list!')
+                if len(duplicate_words) > 0:
+                    if len(duplicate_words) == len(words_to_add):
+                        messages.error(request, f'All the words you tried to add are already on the list!')
+                    else:
+                        messages.error(request, f'The word(s) {duplicate_words} are already on the list')
+            return redirect('/')
     else:
         taboo_form = TabooForm()
 
@@ -30,16 +47,33 @@ def del_word(request):
     if request.method == "POST":
         taboo_form = TabooForm(request.POST)
         if taboo_form.is_valid():
-            t = taboo_form.save(commit = False)
-            word_to_del = t.word
-            match_qs = Taboo.objects.all().filter(word = word_to_del)
-            if match_qs.count() < 1:
-                messages.error(request, f'The word you entered does not exist on the list')
+            t = taboo_form.save(commit=False)
+            words_to_del = t.word.split()
+            if len(words_to_del) == 1:
+                match_qs = Taboo.objects.all().filter(word=words_to_del[0])
+                if match_qs.count() == 0:
+                    messages.error(request, f'The word {words_to_del[0]} does not exist on the list')
+                else:
+                    match_qs.delete()
+                    messages.success(request, f'The word {words_to_del[0]} has been deleted off the list')
             else:
-                match_qs.delete()
-                messages.success(request, f'The word {word_to_del} has been deleted off the list')
-                return redirect('/')
-
+                non_exist_words = list()
+                deleted_words = list()
+                for w in words_to_del:
+                    match_qs = Taboo.objects.all().filter(word=w)
+                    if match_qs.count() != 0:
+                        deleted_words.append(w)
+                        match_qs.delete()
+                    else:
+                        non_exist_words.append(w)
+                if len(deleted_words) > 0:
+                        messages.success(request, f'The word(s) {deleted_words} have been deleted off the list')
+                if len(non_exist_words) > 0:
+                    if len(non_exist_words) == len(words_to_del):
+                        messages.error(request, f'All the words you tried to delete do not exist on the list')
+                    else:
+                        messages.error(request, f'The word(s) {non_exist_words} do not exist on the list')
+            return redirect('/')
     else:
         taboo_form = TabooForm()
 
@@ -51,4 +85,3 @@ def get_words(request):
     words_qs = Taboo.objects.all()
     context = {"words_qs": words_qs}
     return render(request, "taboo/taboo_list.html", context)
-
